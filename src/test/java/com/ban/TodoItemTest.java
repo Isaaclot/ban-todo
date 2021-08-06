478908ea8b971752eb0539f149418e61dc09e129
liangqian <>
2021-08-06 21:26:30 +0800

    Initial commit

---
+++ src/test/java/com/ban/TodoItemTest.java
@@ -0,0 +1,18 @@
+package com.ban;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author liangqian@vvic.com
+ * @version 1.0
+ * @since 2021/8/5
+ */
+public class TodoItemTest {
+
+    @Test
+    public void should_add_item_todo() {
+        assertThat(1).isEqualTo(1);
+    }
+}
